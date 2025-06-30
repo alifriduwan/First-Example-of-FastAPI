2252389170dee3a66312486ac459e39d933f7dd1
Alifriduwan <6510110565@email.psu.ac.th>
2025-06-30 15:30:18 +0700

    Method POST

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -12,3 +12,8 @@ def hello_world():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/items")
+async def create_item(request: Request):
+    body = await request.json()
+    return { "request body" : body }
