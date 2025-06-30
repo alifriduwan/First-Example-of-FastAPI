e272e637ee8a607d837107cb5382b4023b6c3cbf
Alifriduwan <6510110565@email.psu.ac.th>
2025-06-30 15:43:33 +0700

    ใช้ pydantic สำหรับการ validation

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Student(BaseModel):
+    firstName : str
+    lastName: str
+    age : int
 
 @app.get("/")
 def hello_world():
@@ -17,3 +22,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 async def create_item(request: Request):
     body = await request.json()
     return { "request body" : body }
+
+@app.post("/students")
+def create_stuent(student: Student):
+    return { "request body" : student }
