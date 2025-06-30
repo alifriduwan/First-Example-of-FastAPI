ecc1cd8bc636a0d65b9299e4ff7e26ee9d168426
Alifriduwan <6510110565@email.psu.ac.th>
2025-06-30 15:25:42 +0700

    query ใน method GET

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def hello_world():
     return { "message" : "Hello World" }
 
 @app.get("/items/{item_id}")
-def read_item(item_id:int):
-    return {"item_id": item_id,}
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
