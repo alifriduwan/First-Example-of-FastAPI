21bdb64928df86fb1070df7bebb7f6e67b5c90e6
Alifriduwan <6510110565@email.psu.ac.th>
2025-06-30 17:04:10 +0700

    Database ORM config

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from typing import Union
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
+from sqlalchemy import create_engine, Column, Integer, String, Float, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+
+
 app = FastAPI()
 
+# ตั้งค่าและเชื่อมต่อกับฐานข้อมูล SQLite
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 class Student(BaseModel):
     firstName : str
     lastName: str
