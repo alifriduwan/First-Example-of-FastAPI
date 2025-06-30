a64b9fbac581871605792e811799a9bfc7eb001b
Alifriduwan <6510110565@email.psu.ac.th>
2025-06-30 15:11:24 +0700

    การรับ param ใน Method GET

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return { "message" : "Hello World" }
+
+@app.get("/items/{item_id}")
+def read_item(item_id:int):
+    return {"item_id": item_id,}
